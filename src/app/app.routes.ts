123a34c391b6a392fe188c64f790d27fd8f14761
Franco-Villanueva <franco.villanueva.emanuel@gmail.com>
2024-11-19 19:11:55 -0300

    sign in/up auth

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,31 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { TablesComponent } from './Components/tables/tables.component'
+import { TablesComponent } from './Components/tables/tables.component';
 import { SettingComponent } from './Components/setting/setting.component';
 import { GridListComponent } from './Components/Grid/grid-list/grid-list.component';
 
 export const routes: Routes = [
-    { path: 'home', component: GridListComponent },
-    { path: 'table', component: TablesComponent },
-    { path: 'settings', component: SettingComponent },
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { 
+        path: 'home', 
+        component: GridListComponent 
+    },
+    { 
+        path: 'table', 
+        component: TablesComponent 
+    },
+    { 
+        path: 'settings', 
+        component: SettingComponent 
+    },
+
+    
+    { 
+        path: 'auth', 
+        loadChildren: () => import('./Auth/auth.routes')
+    },
+    { 
+        path: '', 
+        redirectTo: 'home', 
+        pathMatch: 'full' 
+    },
 ];
