bd1199a4f96246922c16894702393721f920ac6b
Franco-Villanueva <franco.villanueva.emanuel@gmail.com>
2024-11-28 22:34:31 -0300

    Pruebas FormGroup

--- src/app/Auth/sign-up/sign-up.component.ts
+++ src/app/Auth/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,6 +8,16 @@ import { Component } from '@angular/core';
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
-export default class SignUpComponent {
-
+export default class SignUpComponent implements OnInit {
+  formulario: FormGroup;
+  constructor(private formBuilder: FormBuilder) { }
+  ngOnInit(): void {
+    // Creamos el formulario reactivo con los campos y validaciones
+    this.formulario = this.formBuilder.group({
+      nombre: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      telefono: ['', Validators.pattern('[0-9]{10}')],
+    });
+  }
+  // Aquí irían los métodos para manejar el envío del formulario y las validaciones
 }
