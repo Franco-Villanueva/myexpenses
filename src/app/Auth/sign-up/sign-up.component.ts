276fd6d54bf4242d8b092234ab5cc4e201c9f277
Franco-Villanueva <franco.villanueva.emanuel@gmail.com>
2024-12-03 18:15:32 -0300

    agg Validators a los inputs con estilos

--- src/app/Auth/sign-up/sign-up.component.ts
+++ src/app/Auth/sign-up/sign-up.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -9,25 +9,47 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
   styleUrl: './sign-up.component.css'
 })
 export default class SignUpComponent {
-  userForm: FormGroup;
-  name: FormControl;
-  correo: FormControl;
-  password: FormControl;
 
-  constructor() {
-    this.name = new FormControl('');
-    this.correo = new FormControl('');
-    this.password = new FormControl('');
+  fb = inject(FormBuilder)
 
-    this.userForm = new FormGroup({
-      name: this.name,
-      correo: this.correo,
-      password: this.password
-      
-    })
+  form: FormGroup = this.fb.group({
+    name: [
+      '', 
+      [
+        Validators.required, 
+        Validators.pattern('^[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ]+(?:[\\s-][a-zA-ZáéíóúÁÉÍÓÚüÜñÑ]+){1,5}$')
+      ]
+    ],
+    correo: [
+      '', 
+      [
+        Validators.required, 
+        Validators.email
+      ]
+    ],
+    password: [
+      '', 
+      [
+        Validators.required, 
+        Validators.minLength(8)
+      ]
+    ],
+    password2: [
+      '', 
+      [
+        Validators.required
+      ]
+    ]
+  }, { validator: this.passwordMatchValidator });
+
+  passwordMatchValidator(group: FormGroup) {
+    const password = group.get('password')?.value;
+    const password2 = group.get('password2')?.value;
+    return password === password2 ? null : { notMatching: true };
   }
-  
-  handlerSubmit():void {
-    console.log(this.userForm.value)
+
+  onSubmit():void {
+    console.log('Form Submitted ->>>',this.form.value)
   }
+
 }
--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -1,12 +1,13 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { ToggleComponent } from '../shared/toggle/toggle.component';
 import { LogInComponent } from "../shared/log-in/log-in.component";
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [ToggleComponent, LogInComponent],
+  imports: [ToggleComponent, LogInComponent, RouterLink, RouterLinkActive],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
