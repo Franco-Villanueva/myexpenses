c122e5c4134e6bce67a23431ea2afaea8d468792
Franco-Villanueva <franco.villanueva.emanuel@gmail.com>
2024-12-20 19:28:08 -0300

    agg modal para tener form

--- src/app/Components/tables/form-movement/form-movement.component.ts
+++ src/app/Components/tables/form-movement/form-movement.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { ModalComponent } from '../modal/modal.component';
 
 @Component({
   selector: 'app-form-movement',
   standalone: true,
-  imports: [],
+  imports: [ModalComponent],
   templateUrl: './form-movement.component.html',
   styleUrl: './form-movement.component.css'
 })
 export class FormMovementComponent {
+  isOpen = false
+
+  handlerClick() {
+    this.isOpen = true;
+  }
 
 }
