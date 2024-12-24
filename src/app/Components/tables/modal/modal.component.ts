8fa6e4d143ab7f251676f1cb90da3950c0a0450e
Franco-Villanueva <franco.villanueva.emanuel@gmail.com>
2024-12-23 23:03:18 -0300

    form para agg movimiento

--- src/app/Components/tables/modal/modal.component.ts
+++ src/app/Components/tables/modal/modal.component.ts
@@ -1,14 +1,56 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators,ReactiveFormsModule } from '@angular/forms';
+import { toast } from 'ngx-sonner';
+
 
 @Component({
   selector: 'app-modal',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './modal.component.html',
-  styleUrl: './modal.component.css'
+  styleUrl: './modal.component.css',
 })
 export class ModalComponent {
   @Input() value: boolean = false;
+  _formBuilder = inject(FormBuilder);
+
+  categoriasGasto = [
+    'Comida',
+    'Transporte',
+    'Entretenimiento',
+    'Salud',
+    'Otros',
+  ];
+  categoriasIngreso = [
+    'Salario', 
+    'Ventas', 
+    'Inversiones', 
+    'Regalos', 
+    'Otros'
+  ];
 
 
+  form: FormGroup = this._formBuilder.group(
+    {
+      tipoActividad: ['', [Validators.required]],
+      categoria: ['', [Validators.required]],
+      monto: ['', [Validators.required]],
+    }
+  );
+
+   async onSubmit() {
+      if(this.form.invalid) return;
+  
+      try {
+  
+        const { tipoActividad, categoria, monto } = this.form.value;
+  
+        toast.success('Actividad creada con exito')
+  
+      } catch (error) {
+        console.log(error)
+        toast.error('Ocurrio un error')
+      }
+  
+    }
 }
